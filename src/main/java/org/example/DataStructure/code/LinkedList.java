6c36bce3488b3873302cdf7853d140fd158ac6d3
park <qkr970504@gmail.com>
2024-02-17 08:25:14 +0900

    Linked List 예제 ~ing

---
+++ src/main/java/org/example/DataStructure/code/LinkedList.java
@@ -0,0 +1,14 @@
+package org.example.DataStructure.code;
+
+public class LinkedList {
+}
+
+class Node {
+    int data;
+    Node next;
+
+    public Node(int data) {
+        this.data = data;
+        this.next = null;
+    }
+}
