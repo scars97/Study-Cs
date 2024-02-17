c6690f8286652074959b7a0afdad8700ab01dad5
park <qkr970504@gmail.com>
2024-02-18 08:44:14 +0900

    Linked List add 메서드 추가

--- src/main/java/org/example/DataStructure/code/LinkedList.java
+++ src/main/java/org/example/DataStructure/code/LinkedList.java
@@ -1,6 +1,25 @@
 package org.example.DataStructure.code;
 
 public class LinkedList {
+    private Node head;
+
+    public LinkedList() {
+        this.head = null;
+    }
+
+    // 요소 추가
+    public void add(int data) {
+        Node newNode = new Node(data);
+        if (head == null) {
+            head = newNode;
+        } else {
+            Node current = head;
+            while (current.next != null) {
+                current = current.next;
+            }
+            current.next = newNode;
+        }
+    }
 }
 
 class Node {
