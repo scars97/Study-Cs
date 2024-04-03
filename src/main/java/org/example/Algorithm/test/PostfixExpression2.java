eafd31d76512124f93a8e24a3a17a4bab1d9241c
park <qkr970504@gmail.com>
2024-04-03 18:04:10 +0900

    [baekjoon] 후위표현식2 예제1 구현 - 확장성 없는 구현

--- src/main/java/org/example/Algorithm/test/PostfixExpression2.java
+++ src/main/java/org/example/Algorithm/test/PostfixExpression2.java
@@ -3,45 +3,67 @@ package org.example.Algorithm.test;
 import java.util.LinkedList;
 import java.util.Queue;
 import java.util.Scanner;
+import java.util.Stack;
 
 public class PostfixExpression2 {
+
+    private static final String MULTIPLICATION = "*";
+    private static final String PLUS = "+";
+    private static final String MINUS = "-";
+    private static final String DIVISION = "/";
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
         int N = sc.nextInt();
 
-        String operands = operandMapper(N, sc);
+        // ABC*+DE/- -> (A+(B*C))-(D/E)
+        Double result = result(N, sc);
 
-        System.out.println(operands);
+        System.out.println(result);
         sc.close();
     }
 
-    private static String operandMapper(int N, Scanner sc) {
+    private static Double result(int N, Scanner sc) {
         String postExpression = sc.next();
 
-        Queue<String> q = new LinkedList<>();
+        Queue<Integer> q = new LinkedList<>();
         for (int i = 0; i < N; i++) {
-            String operand = sc.next();
-            q.add(operand);
+            q.add(sc.nextInt());
         }
 
-        int length = postExpression.length();
-        String[] operandArray = new String[length];
-        for (int i = 0; i < operandArray.length; i++) {
+        Stack<Double> stack = new Stack<>();
+        //ABC*+DE/-
+        for (int i = 0; i < postExpression.length(); i++) {
             char c = postExpression.charAt(i);
-            if (Character.isLetter(c)) {
-                operandArray[i] = q.poll();
+            if (!Character.isLetter(c)) {
+                String operator = String.valueOf(c);
+                Double y = stack.pop();
+                Double x = stack.pop();
+
+                Double result = calculation(operator, x, y);
+                stack.add(result);
             } else {
-                operandArray[i] = String.valueOf(c);
+                Double v = Double.valueOf(q.remove());
+                stack.add(v);
             }
         }
 
-        StringBuilder sb = new StringBuilder();
-        for (String s : operandArray) {
-            sb.append(s);
-        }
+        return stack.pop();
+    }
 
-        return sb.toString();
+    private static Double calculation(String operator, double x, double y) {
+
+        if (PLUS.equals(operator)) {
+            return x + y;
+        } else if (MINUS.equals(operator)) {
+            return x - y;
+        } else if (MULTIPLICATION.equals(operator)) {
+            return x * y;
+        } else if (DIVISION.equals(operator)) {
+            return x / y;
+        } else {
+            return null;
+        }
     }
 }
