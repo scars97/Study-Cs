f86dd23aef9f20c0a2d7e2ad7ef5883c2d75de42
park <qkr970504@gmail.com>
2024-04-16 09:18:27 +0900

    예외 처리 - checked 예외

---
+++ src/main/java/org/example/Java/exception/basic/checked/MyCheckedException.java
@@ -0,0 +1,8 @@
+package org.example.Java.exception.basic.checked;
+
+public class MyCheckedException extends Exception{
+
+    public MyCheckedException(String message) {
+        super(message);
+    }
+}
