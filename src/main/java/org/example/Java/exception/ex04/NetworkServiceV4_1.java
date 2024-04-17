1f640c4f32aa582969d0441cd4a5b64fe10ff84d
park <qkr970504@gmail.com>
2024-04-17 09:08:13 +0900

    예외 계층화1

---
+++ src/main/java/org/example/Java/exception/ex04/NetworkServiceV4_1.java
@@ -0,0 +1,25 @@
+package org.example.Java.exception.ex04;
+
+import org.example.Java.exception.ex03.NetworkClientExceptionV3;
+import org.example.Java.exception.ex04.exception.ConnectExceptionV4;
+import org.example.Java.exception.ex04.exception.SendExceptionV4;
+
+public class NetworkServiceV4_1 {
+
+    public void sendMessage(String data) {
+        String address = "http://example.com";
+        NetworkClientV4 client = new NetworkClientV4(address);
+        client.initError(data);
+
+        try {
+            client.connect();
+            client.send(data);
+        } catch (ConnectExceptionV4 e) {
+            System.out.println("[연결 오류] 주소: " + e.getAddress() + ", 메시지: " + e.getMessage());
+        } catch (SendExceptionV4 e) {
+            System.out.println("[전송 오류] 전송 데이터 : " + e.getSendData() + ", 메시지: " + e.getMessage());
+        } finally {
+            client.disconnect();
+        }
+    }
+}
---
+++ src/main/java/org/example/Java/exception/ex04/exception/NetworkClientExceptionV4.java
@@ -0,0 +1,7 @@
+package org.example.Java.exception.ex04.exception;
+
+public class NetworkClientExceptionV4 extends Exception{
+    public NetworkClientExceptionV4(String message) {
+        super(message);
+    }
+}
