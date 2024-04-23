c0a8b1226c59e54793e74f8284d489642acd199c
박성현 <qkr970504@gmail.com>
2024-04-24 08:16:32 +0900

    정적 중첩 클래스

--- src/main/java/org/example/Java/code/Nested/Outer.java
+++ src/main/java/org/example/Java/code/Nested/Outer.java
@@ -2,7 +2,10 @@ package org.example.Java.code.Nested;
 
 public class Outer {
 
+    int outerField = 1;
+
     class Inner {
+        int innerField = 2;
         class Local {
             public void localMethod() {
                 System.out.println("Local.localMethod");
@@ -11,6 +14,7 @@ public class Outer {
     }
 
     static class Static {
+        int staticField = 3;
         public void staticMethod() {
             System.out.println("Static.staticMethod");
         }
