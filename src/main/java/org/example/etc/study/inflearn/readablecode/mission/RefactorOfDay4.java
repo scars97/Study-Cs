ee414319ade9dd25126a06689294e7cf6329b119
박성현 <qkr970504@gmail.com>
2024-10-02 22:40:43 +0900

    [inflearn-warming-up] day04-mission : Early return

--- src/main/java/org/example/etc/study/inflearn/readablecode/mission/RefactorOfDay4.java
+++ src/main/java/org/example/etc/study/inflearn/readablecode/mission/RefactorOfDay4.java
@@ -10,6 +10,28 @@ public class RefactorOfDay4 {
         boolean customerInfo = true;
 
         Order order = new Order(items, totalPrice, customerInfo);
+        System.out.println(validateOrder(order));
+    }
 
+    static boolean validateOrder(Order order) {
+        if (order.getItems().size() == 0) {
+            System.out.println("주문 항목이 없습니다.");
+            return false;
+        }
+
+        if (order.getTotalPrice() > 0) {
+            if (!order.hasCustomerInfo()) {
+                System.out.println("사용자 정보가 없습니다.");
+                return false;
+            }
+            return true;
+        }
+
+        if (!(order.getTotalPrice() > 0)) {
+            System.out.println("올바르지 않은 총 가격입니다.");
+            return false;
+        }
+
+        return true;
     }
 }
