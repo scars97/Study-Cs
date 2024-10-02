9381547313e3955aa74057363e96405351342243
박성현 <qkr970504@gmail.com>
2024-10-02 22:48:59 +0900

    [inflearn-warming-up] day04-mission : 리팩토링

--- src/main/java/org/example/etc/study/inflearn/readablecode/mission/RefactorOfDay4.java
+++ src/main/java/org/example/etc/study/inflearn/readablecode/mission/RefactorOfDay4.java
@@ -12,26 +12,28 @@ public class RefactorOfDay4 {
         Order order = new Order(items, totalPrice, customerInfo);
         System.out.println(validateOrder(order));
     }
-
+    
     static boolean validateOrder(Order order) {
-        if (order.getItems().size() == 0) {
-            System.out.println("주문 항목이 없습니다.");
-            return false;
-        }
+        // 사용자가 생성한 주문이므로 사용자 유효성 검사가 가장 먼저 이뤄져야 함.
+        // 사용자 정보가 없으면 주문 데이터 자체가 성립 되지 않음.
+        if (order.hasCustomerInfo()) {
 
-        if (order.getTotalPrice() > 0) {
-            if (!order.hasCustomerInfo()) {
-                System.out.println("사용자 정보가 없습니다.");
+            if (order.getItems().size() == 0) {
+                System.out.println("주문 항목이 없습니다.");
                 return false;
             }
+
+            // 부정 연산자 대신 부등호 방향 변경
+            // order.getTotalPrice() > 0 조건은 정상이므로 검사 제외
+            if (order.getTotalPrice() < 0) {
+                System.out.println("올바르지 않은 총 가격입니다.");
+                return false;
+            }
+
             return true;
         }
-
-        if (!(order.getTotalPrice() > 0)) {
-            System.out.println("올바르지 않은 총 가격입니다.");
-            return false;
-        }
-
-        return true;
+        
+        System.out.println("사용자 정보가 없습니다.");
+        return false;
     }
 }
